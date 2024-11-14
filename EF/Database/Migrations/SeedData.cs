5a911b6c787628e0a8a3cd1ddd8e12b22782a5a2
Lisa <dimarcolisa@outlook.com>
2024-11-14 16:57:32 +0100

    Update SeedData.cs

--- EF/Database/Migrations/SeedData.cs
+++ EF/Database/Migrations/SeedData.cs
@@ -1,1 +1,14 @@
-
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider, LabDbContext context)
+    {
+        if (!context.LabTests.Any())
+        {
+            context.LabTests.AddRange(
+                new LabTest { TestName = "Emocromo", ReferenceMin = 12.5, ReferenceMax = 16.5 }
+                // Aggiungi altri test
+            );
+            context.SaveChanges();
+        }
+    }
+}
