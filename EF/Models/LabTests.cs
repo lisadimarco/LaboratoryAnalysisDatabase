b6dc84c5c46170f6e86017c8d184db3fa36f6093
Lisa <dimarcolisa@outlook.com>
2024-11-14 17:00:59 +0100

    Create LabTests.cs
    Modello di dati che rappresenta la tabella LabTests.

---
+++ EF/Models/LabTests.cs
@@ -0,0 +1,8 @@
+public class LabTest
+{
+    public int TestId { get; set; }
+    public string TestName { get; set; }
+    public decimal ReferenceMin { get; set; }
+    public decimal ReferenceMax { get; set; }
+    public string Unit { get; set; }
+}
